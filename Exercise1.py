ac5e222afd5c936a4f2440f54378b358d0600e9e
katelynrm <37051008+katelynrm@users.noreply.github.com>
2018-10-05 19:31:11 -0600

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 10:40:44 2018
+
+@author: katel
+"""
+name = 'Katelyn'
+iceCreamRating = 9
+sleepingIndex = 0.9
+
+print('My name is', name, 'and I give eating ice cream a score of', iceCreamRating, 'out of 10!')
+print('I am', name, 'and my sleeping enjoyment index is', sleepingIndex, 'out of 1.0!')
