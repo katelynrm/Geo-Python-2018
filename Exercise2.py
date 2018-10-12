10c887f072c28593204bcecc4f1f9751275fb491
katelynrm <katelynrm@hotmail.com>
2018-10-11 19:53:37 -0600

    First error corrected

--- Exercise2.py
+++ Exercise2.py
@@ -4,3 +4,19 @@ Created on Sun Oct  7 13:03:00 2018
 
 @author: katel
 """
+# First edit is removing ', ' between Kumpula and lighthouse
+stationNames = ['Harmaja', 'Kaisaniemi', 'Kaivopuisto', 'Kumpula lighthouse', \
+                'Malmi airfield', 'Suomenlinna aaltopoiju', 'Vuosaari harbour']
+
+
+stationStartYears = [1989, 1844, 1904, 2005, 2003, 1937, 2016, 2012]
+
+selectedStation = 1
+
+# DO NOT EDIT THIS CELL
+stationIndex = stationNames.index(selectedStation)
+
+stationYears = 2018 - stationStartYears[selectedStation]
+
+print("The Helsinki", selectedStation, "station has been operational for", \
+      stationYears, "years.)
