aea3e000d00380b0b07dbf14d5607a98dbbc0f01
katelynrm <katelynrm@hotmail.com>
2018-10-18 13:44:39 -0600

    Solved prob 1

--- Exercise3.py
+++ Exercise3.py
@@ -4,4 +4,16 @@ Created on Sat Oct 13 09:04:25 2018
 
 @author: katel
 """
+###########
+#PROBLEM 1#
+###########
+basename = "Station"
 
+#create an empty list
+filenames = []
+
+for i in range(21):
+    filenames.append(basename+"_"+str(i)+".txt")
+    
+    
+print(filenames)
