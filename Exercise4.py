c6d0858b1843d7599270c9b864a6019405d1e703
katelynrm <katelynrm@hotmail.com>
2018-10-22 21:09:54 -0600

    Solved problem 2

--- Exercise4.py
+++ Exercise4.py
@@ -13,23 +13,22 @@ def fahrToC(tempF):
 
 
 ###########
-#PROBLEM 2#
+#PROBLEM 2# Creat if else statement
 ###########
-
-if tempC <-2:
-    print(0)
-elif tempC >= -2 and tempC <2:
-    print(1)
-elif tempC >=2 and tempC <15:
-     print(2)
-else:
-     print(3)
-
-
-
-
+def temp_classifier(tempC):
+    if tempC <-2:
+        return 0
+    elif tempC >= -2 and tempC <2:
+        return 1
+    elif tempC >=2 and tempC <15:
+        return 2
+    else:
+        return 3
 
 
 ###########
-#PROBLEM 3#
+#PROBLEM 3# Classify temps
 ###########
+     
+tempData = [19, 21, 21, 21, 23, 23, 23, 21, 19, 21, 19, 21, 23, 27, 27, 28, 30, 30, 32, 32, 32, 32, 34, 34,
+             34, 36, 36, 36, 36, 36, 36, 34, 34, 34, 34, 34, 34, 32, 30, 30, 30, 28, 28, 27, 27, 27, 23, 23, ]
