da0eb8fcf89a66dc1e6c5a195e6d22b1af5d80ce
katelynrm <katelynrm@hotmail.com>
2018-10-22 20:31:30 -0600

    Solved problem 1

---
+++ Exercise4.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 15:03:57 2018
+
+@author: katel
+"""
+
+###########
+#PROBLEM 1#  Creat a temp coverter. Fahrensheit to Celsius
+###########
+def fahrToC(tempF):
+    return (tempF-32)*(5/9)
+
+
+
+
+###########
+#PROBLEM 2#
+###########
+
+
+
+
+
+
+
+###########
+#PROBLEM 3#
+###########
