d020a2a745fb76f2044a2d383eac8338026e3d12
katelynrm <katelynrm@hotmail.com>
2018-10-22 20:57:49 -0600

    Solved problem 2

--- Exercise4.py
+++ Exercise4.py
@@ -12,12 +12,18 @@ def fahrToC(tempF):
     return (tempF-32)*(5/9)
 
 
-
-
 ###########
 #PROBLEM 2#
 ###########
 
+if tempC <-2:
+    print(0)
+elif tempC >= -2 and tempC <2:
+    print(1)
+elif tempC >=2 and tempC <15:
+     print(2)
+else:
+     print(3)
 
 
 
