92bc86af1827243a9516e1edcb7b9840315d419c
katelynrm <katelynrm@hotmail.com>
2018-11-21 12:49:24 -0700

    E-1

---
+++ AutoGIS/Exercise1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from shapely.geometry import Point, LineString, Polygon 
+
+def create_point_geom(x_cord, y_cord):
+    return Point(x_cord, y_cord)
